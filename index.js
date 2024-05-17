5fea59d57ef61f77dce016006d1c23889375e9db
rehankarol09 <rehankarol09@gmail.com>
2024-05-17 17:59:32 +0530

    ADD TEST API

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get("/",(req,res)=>{
 console.log("Test",process.env.PORT)    
 return res.send("Hello");
 })
+app.get("/first",(req,res)=>{
+    return res.send(`<h1>Hello</h1>`)
+})
 
 app.listen(process.env.PORT || 8000,()=>{
     console.log("Server connected",process.env.PORT)
